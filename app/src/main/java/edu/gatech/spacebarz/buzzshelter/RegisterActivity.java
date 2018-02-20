2c72afbac41a2af0fcce285141f8360571d24a08
nschwerd <nathan@schwerdfeger.com>
2018-02-20 10:29:16 -0500

    Started adding functionality, need to push to rebase to get Firebase login

--- app/src/main/java/edu/gatech/spacebarz/buzzshelter/RegisterActivity.java
+++ app/src/main/java/edu/gatech/spacebarz/buzzshelter/RegisterActivity.java
@@ -2,6 +2,9 @@ package edu.gatech.spacebarz.buzzshelter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //localize fields
+        nameView = findViewById(R.id.input_name);
+        emailView = findViewById(R.id.input_email);
+        passwordView = findViewById(R.id.input_password);
+        phoneView = findViewById(R.id.input_phoneNumber);
+        userTypeSpinner = findViewById(R.id.spinner_userType);
+
+        //Spinner
     }
+
+    private AutoCompleteTextView nameView;
+    private AutoCompleteTextView emailView;
+    private EditText passwordView;
+    private EditText phoneView;
+    private Spinner userTypeSpinner;
 }
