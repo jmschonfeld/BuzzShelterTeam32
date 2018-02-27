1aa04b851fa561883c89b0da0b327e81fc1dd716
jmschonfeld <jeremyschonfeld@gmail.com>
2018-02-26 20:04:26 -0500

    Break UserInfo and UserRole into separate file

--- app/src/main/java/edu/gatech/spacebarz/buzzshelter/model/FirebaseDBManager.java
+++ app/src/main/java/edu/gatech/spacebarz/buzzshelter/model/FirebaseDBManager.java
@@ -8,7 +8,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseException;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.IgnoreExtraProperties;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.concurrent.CountDownLatch;
@@ -140,52 +139,6 @@ public class FirebaseDBManager {
 
     }
 
-    @IgnoreExtraProperties
-    public static class UserInfo {
-        private String uid, name, phone;
-        private UserRole role;
-
-        /** Required for use with Firebase, should not be used by the app */
-        public UserInfo() {
-
-        }
-
-        public UserInfo(String u, String n, String p, UserRole r) {
-            uid = u;
-            name = n;
-            phone = p;
-            role = r;
-        }
-
-        public String getUid() {
-            return uid;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public String getPhone() {
-            return phone;
-        }
-
-        public UserRole getRole() {
-            return role;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public void setPhone(String phone) {
-            this.phone = phone;
-        }
-
-        public void setRole(UserRole role) {
-            this.role = role;
-        }
-    }
-
     private enum DatabaseKey {
         USER("SSUser"), SHELTER("SSShelter");
 
@@ -197,39 +150,6 @@ public class FirebaseDBManager {
         @Override
         public String toString() {
             return key;
-        }
-    }
-
-    public enum UserRole {
-        USER("User"), ADMINISTRATOR("Administrator"), SHELTER_EMPLOYEE("Shelter Employee");
-
-        UserRole(String s) {
-            label = s;
-        }
-        private String label;
-
-        @Override
-        public String toString() {
-            return label;
-        }
-
-        public static UserRole findUserRole(String id) {
-            try {
-                UserRole role = UserRole.valueOf(id);
-                if (role != null) {
-                    return role;
-                }
-            } catch (IllegalArgumentException e) {
-                // Intentional fallthrough
-            }
-
-            for (UserRole r : UserRole.values()) {
-                if (r.label.equals(id)) {
-                    return r;
-                }
-            }
-
-            return null;
         }
     }
 
