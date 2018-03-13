688fee96ce3808276de1c0569af393c159b2471a
jmschonfeld <jeremyschonfeld@gmail.com>
2018-03-13 12:39:24 -0400

    Remove unused import

--- app/src/main/java/edu/gatech/spacebarz/buzzshelter/util/ShelterListAdapter.java
+++ app/src/main/java/edu/gatech/spacebarz/buzzshelter/util/ShelterListAdapter.java
@@ -12,7 +12,6 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.TwoLineListItem;
 
 import java.io.Serializable;
 import java.util.ArrayList;
